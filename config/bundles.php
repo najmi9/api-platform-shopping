e7ad4f465f9e03fd8c1d1edbecbcbec80564bdfb
“Imad <imadnajmi9@gmail.com>
2020-07-05 00:08:27 +0100

    x

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all'=>true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev'=>true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
